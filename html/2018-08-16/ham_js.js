0081252f3a86a7f3348654a9d6dc9ed89fd1e5e5
thienpn1 <thienpn1@gmail.com>
2018-08-18 20:03:17 +0700

    upcode ngay 18

--- html/2018-08-16/ham_js.js
+++ html/2018-08-16/ham_js.js
@@ -1,19 +1,12 @@
-var status=0;
-var light=document.getElementById('light')
+var status = 0;
+var light = document.getElementById("light");
 
 function switch_light() {
-   
-    
-    console.log(light);
-    console.log(status);
-    if (status==1) {
-        light.src='./pic_bulboff.gif';
-        status=0;
-    }else if (status==0) {
-        light.src='./pic_bulbon.gif';
-        status=1;
-    } 
-    
-   
-
+ var temp=light.src;
+ if (temp.search('bon')!=-1) {
+    light.src='./pic_bulboff.gif'
+     console.log(temp.search('bon'))
+ } else {
+    light.src='./pic_bulbon.gif'
+ }
 }
---
+++ html/2018-08-18/ham_js.js
@@ -0,0 +1,12 @@
+
+
+function test() {
+    var x = document.getElementById("khung_1");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+
+
+}
