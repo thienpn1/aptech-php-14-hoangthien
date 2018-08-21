9b493230a2eccd4956d2127845d3494296264867
thienpn1 <thienpn1@gmail.com>
2018-08-21 16:22:29 +0700

    upcode album

--- html/2018-08-18/ham_js.js
+++ html/2018-08-18/ham_js.js
@@ -1,12 +1,28 @@
-
-
 function test() {
-    var x = document.getElementById("khung_1");
-    if (x.style.display === "block") {
-        x.style.display = "none";
+    var x = document.getElementById("hidden_div");
+    var sty=x.className;
+    x.height=300;
+    console.log(x.className);
+    var pos = sty.search("d-flex");
+    console.log(pos);
+    if (pos!=-1) {
+        
+        var st = sty.replace("d-flex","d-none");
+        console.log("true "+st);
+        x.className=st;
     } else {
-        x.style.display = "block";
+        var st = sty.replace("d-none","d-flex");
+        console.log("false "+st);
+        x.className=st;
     }
-
+    /*if (x.className == "d-none") {
+        x.className = "d-block";
+        console.log(x.style.transitionDuration);
+        
+    } else {
+        x.className = "d-none";
+        
+    }
+    */
 
 }
