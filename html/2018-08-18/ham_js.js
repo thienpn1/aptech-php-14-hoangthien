4336f01b9b7966357966ca2daa05faa58d26d753
thienpn1 <thienpn1@gmail.com>
2018-08-21 19:32:32 +0700

    upcode album co fixed-top

--- html/2018-08-18/ham_js.js
+++ html/2018-08-18/ham_js.js
@@ -1,8 +1,9 @@
 function test() {
     var x = document.getElementById("hidden_div");
+    var y=document.getElementById("show_div");
     var sty=x.className;
     x.height=300;
-    console.log(x.className);
+    console.log(x.style.display);
     var pos = sty.search("d-flex");
     console.log(pos);
     if (pos!=-1) {
@@ -10,10 +11,18 @@ function test() {
         var st = sty.replace("d-flex","d-none");
         console.log("true "+st);
         x.className=st;
+        y.classList.add("position-fixed");
+        y.classList.add("bg-dark");
+        y.style.top="0";
+        y.style.zIndex=100;
+        
     } else {
         var st = sty.replace("d-none","d-flex");
         console.log("false "+st);
         x.className=st;
+        y.classList.remove("position-fixed");
+        y.classList.remove("bg-dark");
+        y.style.removeProperty("top");
     }
     /*if (x.className == "d-none") {
         x.className = "d-block";
@@ -24,5 +33,5 @@ function test() {
         
     }
     */
-
+   
 }
