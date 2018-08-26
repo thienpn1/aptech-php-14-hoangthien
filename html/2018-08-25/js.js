5ed0fcddd5f8eed597a266da6c9905d416d73866
thienpn1 <thienpn1@gmail.com>
2018-08-27 00:22:34 +0700

    upcode doc json

--- html/2018-08-25/js.js
+++ html/2018-08-25/js.js
@@ -16,22 +16,18 @@ function load() {
 function render(params) {
     console.log(params);
     
-    document.write('<table class="table">');
-    document.write('<thead class="thead-dark">');
-    document.write('<tr>');
-    document.write('  <th>Id</th>');
-    document.write('  <th>Name</th>');
-    document.write('  <th>Email</th>');
-    document.write('</tr>');
-    document.write('</thead>');
+    $('#tbl').append('<thead class="thead-dark" id="cottieude">');
+    $('#cottieude').append('<tr id="tieude">');
+    $('#tieude').append('  <th class="text-center">Id</th>');
+    $('#tieude').append('  <th class="text-center">Name</th>');
+    $('#tieude').append('  <th class="text-center">Email</th>');
     params.users.forEach(element => {
-        
-        document.write('<tr>');
-        document.write('<td>'+element.id+'</td>');
-        document.write('<td>'+element.name+'</td>');
-        document.write('<td>'+element.email+'</td>');
-        document.write('</tr>');
-    });
-  document.write('</table>');
+        $('#tbl').append('<tr id=hang_'+element.id+'>');
+        $('#hang_'+element.id).append('<td class="text-center">'+element.id+'</td>');
+        $('#hang_'+element.id).append('<td class="text-center">'+element.name+'</td>');
+        $('#hang_'+element.id).append('<td class="text-center">'+element.email+'</td>');
+       
+     });
+//   document.write('</table>');
     
 }
